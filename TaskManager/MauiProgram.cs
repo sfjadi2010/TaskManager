7461039fec3ced40a969decff56bbe74f05bd4a4
salman.farsi <salman.farsi@3together.us>
2024-03-10 19:54:42 -0400

    The most significant changes include the modification of the `App` class constructor to accept a `MainView` object, the removal of `AppShell.xaml` and `AppShell.xaml.cs` files indicating a shift away from using the `Shell` class for navigation, and the addition of service, view model, and view registration in the `CreateMauiApp` method in `MauiProgram.cs`. Additionally, a new `StackLayout` has been added to `ItemView.xaml` and the constructor of the `ItemView` class has been modified to accept an `ItemViewModel` object, enabling data binding between the view and the view model.
    1. The `App` class constructor in `App.xaml.cs` has been modified to accept a `MainView` object. The `MainPage` property is now set to a new `NavigationPage` object that wraps the passed `MainView` object, instead of a new `AppShell` object.
    
    2. The `AppShell.xaml` and `AppShell.xaml.cs` files have been completely removed, suggesting that the application is no longer using the `Shell` class for navigation.
    
    3. The `CreateMauiApp` method in `MauiProgram.cs` has been modified to register the `ITaskItemRepository` service, `MainViewModel` and `ItemViewModel` view models, and `MainView` and `ItemView` views with the dependency injection container. This allows these classes to be automatically instantiated and injected where needed in the application.
    
    4. A new `StackLayout` containing a `Switch` and a `Label` has been added to `ItemView.xaml`, suggesting that the `ItemView` now includes a switch for marking a task as completed.
    
    5. The constructor of the `ItemView` class in `ItemView.xaml.cs` has been modified to accept an `ItemViewModel` object. The `Navigation` property of the passed `ItemViewModel` object is set to the `Navigation` property of the `ItemView` object, and the `BindingContext` of the `ItemView` object is set to the passed `ItemViewModel` object. This enables data binding between the view and the view model.

--- TaskManager/MauiProgram.cs
+++ TaskManager/MauiProgram.cs
@@ -1,25 +1,38 @@
 ﻿using Microsoft.Extensions.Logging;
+using TaskManager.Repositories;
+using TaskManager.ViewModels;
+using TaskManager.Views;
 
-namespace TaskManager
+namespace TaskManager;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Register Services
+        builder.Services.AddSingleton<ITaskItemRepository, TaskItemRepository>();
+
+        // Register ViewModels
+        builder.Services.AddTransient<MainViewModel>();
+        builder.Services.AddTransient<ItemViewModel>();
+
+        // Register Views
+        builder.Services.AddTransient<MainView>();
+        builder.Services.AddTransient<ItemView>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
--- TaskManager/Views/ItemView.xaml.cs
+++ TaskManager/Views/ItemView.xaml.cs
@@ -1,9 +1,13 @@
+using TaskManager.ViewModels;
+
 namespace TaskManager.Views;
 
 public partial class ItemView : ContentPage
 {
-	public ItemView()
+	public ItemView(ItemViewModel viewModel)
 	{
 		InitializeComponent();
+		viewModel.Navigation = Navigation;
+		BindingContext = viewModel;
 	}
 }
