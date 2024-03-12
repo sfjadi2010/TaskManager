ac16e9210c0888579a5197e7859b611aaebb0e66
salman.farsi <salman.farsi@3together.us>
2024-03-11 23:50:43 -0400

    The most significant changes were made to the `MainViewModel.cs` and `MainView.xaml` files. In `MainViewModel.cs`, new namespaces were added, a new `ObservableCollection` was created, event handlers were added to the constructor, and two new methods were added for loading data and creating view models. In `MainView.xaml`, a new `ResourceDictionary` was added, and the `ListView` was updated to include a `DataTemplate` for each item. A new file `StatusColorConverter.cs` was also added.
    Here are the changes in detail:
    
    1. `App.xaml`: Two new color resources, `CompletedColor` and `ActiveColor`, were added to the `ResourceDictionary`.
    
    2. `MainViewModel.cs`:
       - The `CommunityToolkit.Mvvm.ComponentModel` and `System.Collections.ObjectModel` namespaces were added.
       - A new `ObservableCollection` of `TaskItemViewModel` named `items` was added.
       - Event handlers for `OnItemAdded` and `OnItemUpdated` events of `taskItemRepository` were added to the `MainViewModel` constructor.
       - A new method `LoadDataAsync` was added to load data from the repository and create view models for each item.
       - A new method `CreateTaskItemViewModel` was added to create a view model for a `TaskItem`.
    
    3. `MainView.xaml`:
       - A new `ResourceDictionary` was added to the `ContentPage.Resources` that includes a `StatusColorConverter`.
       - The `ListView` was updated to include a `DataTemplate` for each item, which includes a `Grid` layout and several `Label` elements. The `BoxView`'s `BackgroundColor` is bound to the `Item.Completed` property using the `StatusColorConverter`.
    
    4. `StatusColorConverter.cs`: A new file was added, which includes a `StatusColorConverter` class that implements `IValueConverter`. This converter returns a color from the application resources based on the boolean value it is given.

---
+++ TaskManager/Converters/StatusColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Converters;
+
+public class StatusColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (Color) Application.Current.Resources[(bool)value ? "CompletedColor" : "ActiveColor"];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
--- TaskManager/ViewModels/MainViewModel.cs
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using TaskManager.Models;
 using TaskManager.Repositories;
 using TaskManager.Views;
 
@@ -8,9 +11,13 @@ public partial class MainViewModel : ViewModel
 {
     private readonly ITaskItemRepository _taskItemRepository;
     private readonly IServiceProvider _serviceProvider;
-
+    [ObservableProperty]
+    ObservableCollection<TaskItemViewModel> items;
     public MainViewModel(ITaskItemRepository taskItemRepository, IServiceProvider serviceProvider)
     {
+        taskItemRepository.OnItemAdded += (sender, item) => items.Add(CreateTaskItemViewModel(item));
+        taskItemRepository.OnItemUpdated += (sender, item) => Task.Run(async() => await LoadDataAsync());
+
         _taskItemRepository = taskItemRepository;
         _serviceProvider = serviceProvider;
         Task.Run(async () => await LoadDataAsync());
@@ -20,6 +27,20 @@ public partial class MainViewModel : ViewModel
     public async Task AddItemAsync() => await Navigation.PushAsync(_serviceProvider.GetRequiredService<ItemView>());
 
     private async Task LoadDataAsync()
+    {
+        var items = await _taskItemRepository.GetItemsAsync();
+        var itemViewModels = items.Select(item => CreateTaskItemViewModel(item));
+        Items = new ObservableCollection<TaskItemViewModel>(itemViewModels);
+    }
+
+    private TaskItemViewModel CreateTaskItemViewModel(TaskItem item)
+    {
+        var taskItemViewModel = new TaskItemViewModel(item);
+        taskItemViewModel.ItemStatusChanged += async (sender, e) => await LoadDataAsync();
+        return taskItemViewModel;
+    }
+
+    private void ItemStatusChanged(object sender, EventArgs e)
     {
 
     }
